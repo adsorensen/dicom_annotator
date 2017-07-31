a36769ca49b3f692b2fc1d2b700fb3a553c71fcc
adsorensen <assassinadam@hotmail.com>
2017-07-31 14:37:46 -0600

    pug file is now pulling options from a dict in main. Save button saves metadata to Girder

--- web_client/main.js
+++ web_client/main.js
@@ -7,6 +7,9 @@ import './annotationSelect.styl';
 import View from 'girder/views/View';
 //import annotationView from './views/view';
 
+var mylist = [{key:"test", value:"testValue"}, {key:"other", value:"otherValue"},
+    {key:"test #2", value:"something"}, {key: "Adam", value: "Sorensen"}];
+
 wrap(DicomView, 'render', function (render) {
     render.call(this);
 
@@ -15,28 +18,30 @@ wrap(DicomView, 'render', function (render) {
     }).done((resp) => {
         this.$('.g-dicom-panes').before('<div class="g-annotation-container"></div>');
         this.$('.g-annotation-container').html(template({
-            domains: resp
+            //domains: resp
+            domains: mylist
         }));
     });
 
     return this;
 });
 
-DicomView.prototype.events['change .g-annotation-domain'] = function (e) {
-    var domain = $(e.currentTarget).val();
-    //alert("here");
-    this.item.editMetadata('annotationDomain', 'annotationDomain', domain, () => {
-        this.item.trigger('g:changed');
-    });
-}
+// DicomView.prototype.events['change .g-annotation-domain'] = function (e) {
+//     var domain = $(e.currentTarget).val();
+//     this.item.editMetadata('annotationDomain', 'annotationDomain', domain, () => {
+//         this.item.trigger('g:changed');
+//     });
+// }
 
 // Event to handle when the user clicks on the add label button
 // Function will grab the selected option from the drop down
 // and add a new option to the labels selection box with the 
-// name and value from the drop down
+// name and value from the drop down. If the value of the label
+// already exists in the select box, it will not add it.
 DicomView.prototype.events['click .add-label'] = function (e) {
     var $label = $('#selection option:selected').text();
     var $value = $('#selection option:selected').val();
+    //alert($value);
     if ($("#labels option[value=" + $value + "]").length > 0)
     {
         alert($label + " already exists");
@@ -54,12 +59,19 @@ DicomView.prototype.events['click .add-label'] = function (e) {
 // Function will take all labels currently in the selection box
 // and add them to the girder item metadata
 DicomView.prototype.events['click .save-labels'] = function (e) {
-    
+    var labels = "";
+    $("#labels > option").each(function() {
+        labels += this.text + ";\n";
+    });
+    labels = labels.slice(0, -2);
+    //alert(labels);
+    this.item.editMetadata('Ontology Tags', 'Ontology Tags', labels, () => {
+        this.item.trigger('g:changed');
+    });
 }
 
 // Event to handle when the user clicks the remove label button
 // Function will remove the selected value from the selection box
 DicomView.prototype.events['click .remove-label'] = function (e) {
     $('#labels option:selected').remove();
-
 }
